f1a79a51c1866334a746fa58972eedc24c1a46e0
Geoff Goodman <ggoodman@gmail.com>
2015-10-08 10:43:47 -0400

    Update webtask transformer

--- adapters/transformers/webtask.js
+++ adapters/transformers/webtask.js
@@ -1,57 +1,82 @@
 var Bluebird = require('bluebird');
 var Boom = require('boom');
 var Query = require('querystring');
-var Wreck = require('wreck');
+var Sandbox = require('sandboxjs');
+var Url = require('url');
+var Wreck = Bluebird.promisifyAll(require('wreck'));
 var _ = require('lodash');
-
-var webtaskToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IjIifQ.eyJqdGkiOiJmODdlNTNkZjc1NTQ0NGM0YjJkY2M2YmJmMmJjMjExMCIsImlhdCI6MTQzNjgwMjc3MSwiY2EiOltdLCJkZCI6MSwidGVuIjoiL15wbHVua2VyLS8ifQ.VAk9_SerUUGd7GlXSUGEgbzidoSyORYAlZxsmFOzsas';
 
 module.exports = {
   matches: /\..+$/,
   provides: ".json",
-  transform: function (context) {
-    var query = _.extend({}, context.request.query, {
-      webtask_url: context.config.shared.url.run
-        + (context.preview.type === 'plunk' ? '/plunks' : '')
-        + '/' + context.preview.id + '/' + context.requestPath.replace(/\.json$/, '.js'),
-    });
-    var url = 'https://webtask.it.auth0.com/api/run/plunker-' + context.preview.id
-      + '?' + Query.stringify(query);
-    var options = {
-      headers: _.extend({}, context.request.headers, {
-        'Authorization': 'Bearer ' + webtaskToken,
-      }),
-      rejectUnauthorized: false,
-      timeout: 1000 * 5,
-    };
-    
-    if (context.request.payload) {
-      options.payload = _.isObject(context.request.payload)
-        ? JSON.stringify(context.request.payload)
-        : context.request.payload;
+  transform: function(context) {
+    var config = context.config;
+    var webtaskToken = _.get(config, 'webtask.token');
+    var webtaskUrl = _.get(config, 'webtask.url');
+
+    return createNamedWebtask()
+      .then(runWebtask);
+
+
+    function createNamedWebtask() {
+      var sandbox = Sandbox.init({
+        url: webtaskUrl,
+        token: webtaskToken,
+        container: 'plunker',
+      });
+
+      var codeUrl = context.config.shared.url.run + (context.preview.type === 'plunk' ? '/plunks' : '') + '/' + context.preview.id + '/' + context.requestPath.replace(/\.json$/, '.js');
+
+      return sandbox.create(codeUrl, {
+        name: context.preview.type + '-' + context.preview.id,
+        parse: false,
+        merge: false,
+      });
     }
     
-    return new Bluebird(function (resolve, reject) {
-      Wreck.request(context.request.method, url, options, function (err, res) {
-        if (err) {
-          return reject(Boom.wrap(err, 502, 'Error communicating with webtask.io: ' + err.message));
-        }
-        
-        if (res.statusCode >= 300) {
-          return Wreck.read(res, {json: true, timeout: 1000, maxBytes: 1024 * 1024}, function (err, payload) {
-            context.preview.log({
-              source: 'webtask',
-              data: payload,
+
+    function runWebtask(webtask) {
+      var url = Url.parse(webtask.url, true);
+      var options = {
+        rejectUnauthorized: false,
+        timeout: 1000 * 5,
+      };
+
+      _.extend(url.query, context.request.query);
+
+      if (context.request.payload) {
+        options.payload = _.isObject(context.request.payload) ? JSON.stringify(context.request.payload) : context.request.payload;
+      }
+
+      return new Bluebird(function(resolve, reject) {
+        Wreck.request(context.request.method, Url.format(url), options, function(err, res) {
+          if (err) {
+            return reject(Boom.wrap(err, 502, 'Error communicating with webtask.io: ' + err.message));
+          }
+
+          if (res.statusCode >= 300) {
+            return Wreck.read(res, {
+              json: true,
+              timeout: 1000,
+              maxBytes: 1024 * 1024
+            }, function(err, payload) {
+              if (err) return reject(err);
+
+              context.preview.log({
+                source: 'webtask',
+                data: payload,
+              });
+
+              reject(Boom.badRequest('Error running code on webtask.io', payload));
             });
-            reject(Boom.badRequest('Error running code on webtask.io', payload));
+          }
+
+          resolve({
+            payload: res,
+            headers: res.headers,
           });
-        }
-        
-        resolve({
-          payload: res,
-          headers: res.headers,
         });
       });
-    });
+    }
   }
 };
